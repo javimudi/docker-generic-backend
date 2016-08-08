bbc245af319ab6363779f0b8543e119b44c32d13
Javier Muñoz <javimudi@users.noreply.github.com>
2016-08-08 19:51:38 +0200

    http basic auth, first code

--- app/app.js
+++ app/app.js
@@ -17,17 +17,26 @@ var debug = {
 var server = express(),
 router = express.Router();
 
+var passport = require('passport'),
+authRoutes = require('./core/auth/routes');
+
 var restServer = function(callback){
 
 	// Basic setup
 	server.use(bodyparser.urlencoded({ extended: true }));
 	server.use(bodyparser.json());
+	server.use(passport.initialize());
 
 
 	// MongoDB connection
 	mongoose.connect(settings.mongodbstring)
 	mongoose.connection.on('connected', function(){
 		debug.info("Connected to DB " + settings.mongodbstring);
+
+
+		// Core/Auth
+		debug.info("Appending auth routes");
+		server.use('/api/'+settings.apiversion, authRoutes);
 
 		// Routes
 		discover().on('newroute', function(routes){
